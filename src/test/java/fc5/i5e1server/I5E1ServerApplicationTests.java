301a1cb6704ed88b6a8943366a4e37a95b7bbea3
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-25 16:33:58 +0900

    [#1] feat: 프로젝트 생성

---
+++ src/test/java/fc5/i5e1server/I5E1ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fc5.i5e1server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I5E1ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
