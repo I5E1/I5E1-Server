0bcaefb12fe900c733ea8afe4fe393c7e56e1633
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-25 17:21:23 +0900

    [#1] feat: Entity 파일 생성 및 연관관계 매핑

---
+++ src/main/java/fc5/i5e1server/domain/model/Member.java
@@ -0,0 +1,42 @@
+package fc5.i5e1server.domain.model;
+
+
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 60, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 12)
+    private String tel;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @ColumnDefault("'STAFF'")
+    private Position position;
+
+    @Column(nullable = false, columnDefinition = "INT UNSIGNED")
+    private int annualCount;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
