414636390b6b7d648fda0ff45d1c85507fb608c6
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-25 16:33:58 +0900

    [#1] feat: 프로젝트 생성

---
+++ src/main/java/fc5/i5e1server/I5E1ServerApplication.java
@@ -0,0 +1,13 @@
+package fc5.i5e1server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I5E1ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I5E1ServerApplication.class, args);
+    }
+
+}
