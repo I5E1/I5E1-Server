4849a7bbfcfe2d206d6f39cff509c98daa984118
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-08-02 19:58:00 +0900

    Revert "[#11] feat: 마이페이지 수정 구현"
    This reverts commit d04a58fd1b681deba7fcbf5d478daad15c0edf0c.

--- src/main/java/fc5/i5e1server/domain/member/MemberController.java
+++ src/main/java/fc5/i5e1server/domain/member/MemberController.java
@@ -4,8 +4,6 @@ import fc5.i5e1server.common.APIDataResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,10 +19,5 @@ public class MemberController {
     public ResponseEntity<APIDataResponse<MemberInfoDTO>> myPage() {
         MemberInfoDTO memberInfoDTO = memberService.getMember(1L);
         return APIDataResponse.of(HttpStatus.OK, "user data", memberInfoDTO);
-    }
-    @PutMapping("api/user")
-    public ResponseEntity<APIDataResponse<Void>> updateMyPage(@RequestBody MemberUpdateReqDTO request) {
-        MemberInfoDTO memberInfoDTO = memberService.updateMember(1L, request);
-        return APIDataResponse.empty(HttpStatus.OK, "updated user data");
     }
 }
