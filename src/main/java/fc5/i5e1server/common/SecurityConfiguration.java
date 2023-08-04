927fe4d925b454347d4a52574c7e6d4820526731
brotherstone97 <brotherstone97@gmail.com>
2023-08-04 11:46:10 +0900

    feat: spring security configuration 생성

--- src/main/java/fc5/i5e1server/common/SecurityConfiguration.java
+++ src/main/java/fc5/i5e1server/common/SecurityConfiguration.java
@@ -1,15 +1,53 @@
 package fc5.i5e1server.common;
 
+import fc5.i5e1server.domain.auth.jwt.JwtAccessDeniedHandler;
+import fc5.i5e1server.domain.auth.jwt.JwtAuthenticationEntryPoint;
+import fc5.i5e1server.domain.auth.jwt.JwtSecurityConfig;
+import fc5.i5e1server.domain.auth.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class SecurityConfiguration {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfiguration{
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.httpBasic().disable().build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+            return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
