508e7c692f2a63c12109fa3287bdb271c5579a7d
brotherstone97 <brotherstone97@gmail.com>
2023-08-07 14:03:14 +0900

    [#20] feat: 이메일 중복검사 기능 구현

--- src/main/java/fc5/i5e1server/domain/member/MemberService.java
+++ src/main/java/fc5/i5e1server/domain/member/MemberService.java
@@ -5,9 +5,11 @@ import fc5.i5e1server.domain.model.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import javax.transaction.Transactional;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +18,8 @@ public class MemberService {
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public Member signUp(JoinDto joinDto){
-        if(memberRepository.findByEmail(joinDto.getEmail()).orElse(null)!=null){
+    public Member signUp(JoinDto joinDto) {
+        if (memberRepository.findByEmail(joinDto.getEmail()).orElse(null) != null) {
             throw new RuntimeException("이미 가입되어 있는 유저입니다.");
         }
         Member member = Member.builder()
@@ -26,16 +28,17 @@ public class MemberService {
                 .password(passwordEncoder.encode(joinDto.getPassword()))
                 .tel(joinDto.getTel())
                 .build();
-         return memberRepository.save(member);
+        return memberRepository.save(member);
     }
-  
-  //@Todo update랑 상동
+
+    //@Todo update랑 상동
     @Transactional
     public MemberInfoDTO getMember(Long id) {
         Member member = memberRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("유저 없음"));
         return new MemberInfoDTO(member);
     }
+
     @Transactional
     //@Todo 로그인 완성후에 //Principal principal
     public MemberInfoDTO updateMember(Long id, MemberUpdateReqDTO request) {
@@ -49,9 +52,13 @@ public class MemberService {
         member.updatePassword(request.getPassword());  // 로그인 완료 후 수정
         return new MemberInfoDTO(member);
     }
+
     public Member findByUserId(Long userId) {
         return memberRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found with userId: " + userId));
     }
 
+    public boolean isDuplicateEmail(String email) {
+        return memberRepository.findByEmail(email).isPresent();
+    }
 }
